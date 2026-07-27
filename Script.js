143bf66b0dc7daa03dcabe6f95d509a8085f15bc
akshatvishwakarma582 <akshatvishwakarma582@gmail.com>
2026-07-27 10:40:30 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,19 @@
+// ===========================
+// Rabico v0.1
+// ===========================
 
+console.log("🐰 Rabico Loaded!");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        button.style.transform = "scale(0.95)";
+
+        setTimeout(() => {
+            button.style.transform = "scale(1)";
+        }, 150);
+
+        console.log(`${button.innerText} clicked`);
+    });
+});
